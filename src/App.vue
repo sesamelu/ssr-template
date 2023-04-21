28cbd6ac81bd34607531ac02f5048e2f013798fe
卢世敏 <shimin.lu@niu.com>
2023-04-21 18:22:06 +0800

    集成element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
   import {useRouter} from 'vue-router'
-  const router = useRouter()
+  import { getCurrentInstance, h } from 'vue'
+  // import { ElMessage } from 'element-plus'
 
+  const router = useRouter()
+  //声明上下文
+  const {proxy}:any = getCurrentInstance()
+  const open = () => {
+    proxy.$message('this is a message.')
+  }
 </script>
 
 <template>
   <div>
+    <el-button type="primary" @click="open">Primary</el-button>
     <button @click="()=>router.push({path:'/home'})">首页</button>
     <button @click="()=>router.push({path:'/mine'})">个人中心</button>
     <router-view></router-view>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import ElementPlus, { ElMessage } from 'element-plus'
+import 'element-plus/dist/index.css'
+
 
 const app = createApp(App)
 app.use(router)
+app.use(ElementPlus)
+app.config.globalProperties.$message = ElMessage
 app.mount('#app')
